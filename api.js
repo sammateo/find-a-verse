0d6d82a38679377296493b8ada2a6d296b4a3b5b
sammateo <75133532+sammateo@users.noreply.github.com>
2021-02-26 14:15:44 -0400

    Add files via upload

--- api.js
+++ api.js
@@ -1,6 +1,32 @@
 const button = document.querySelector('#button');
 
+async function getDaily(){
+    const dayData = await fetch('https://beta.ourmanna.com/api/v1/get/?format=json');
+    let dailyObj = await dayData.json();
+    dailyObj = dailyObj.verse.details;
+    let dailyBook = dailyObj.reference.split(" ");
+    let dailyAddress=dailyBook[1].split(":");
+    dailyBook=dailyBook[0];
+    dailyChapter=dailyAddress[0];
+    dailyVerse=dailyAddress[1];
 
+
+    let book = document.querySelector('#book');
+    let chap = document.querySelector('#chapter');
+    let verse = document.querySelector('#verse');
+
+    book.value=dailyBook;
+    chap.value=Number(dailyChapter);
+    verse.value=dailyVerse;
+    console.log(dailyBook);
+    console.log(dailyChapter);
+    console.log(dailyVerse);
+
+    
+}
+
+
+window.onload = getDaily;
 button.addEventListener('click',getVerses);
 
 async function getVerses(){
@@ -9,6 +35,7 @@ async function getVerses(){
     let verse = document.querySelector('#verse');
     let ref= document.querySelector('#ref-response');
     let text = document.querySelector('#text-response');
+    let respcontain = document.querySelector('#response-container');
     book = book.value;
     chap = chap.value;
     verse= verse.value;
@@ -21,10 +48,10 @@ async function getVerses(){
         i++;
     }
     
-    
-    ref.innerHTML='Reference: '+ bibleObj.reference;
+    respcontain.style.display="block";
+    ref.innerHTML='<h3>Reference:</h3> '+ bibleObj.reference;
     // text.innerHTML='Text: '+ bibleObj.text;
-    text.innerHTML='Text: ' + display;
-    // console.log();
+    text.innerHTML='<h3>Text:</h3> ' + display;
+    console.log(bibleObj);
     
 }
